189071edceec368d8c534ab22935bcacb194cd10
Josh Dennis <josh@joshdennis.net>
2012-07-16 09:58:12 -0500

    add the ability to create an unique URL for every request regardless of whether or not the owner already has shortened this URL

--- app/models/shortener/shortened_url.rb
+++ app/models/shortener/shortened_url.rb
@@ -18,22 +18,24 @@ class Shortener::ShortenedUrl < ActiveRecord::Base
   # generate a shortened link from a url
   # link to a user if one specified
   # throw an exception if anything goes wrong
-  def self.generate!(orig_url, owner=nil)
+  def self.generate!(orig_url, owner=nil, duplicates=false)
     # if we get a shortened_url object with a different owner, generate
     # new one for the new owner. Otherwise return same object
     if orig_url.is_a?(Shortener::ShortenedUrl)
       return orig_url.owner == owner ? orig_url : generate!(orig_url.url, owner)
     end
 
-    # don't want to generate the link if it has already been generated
-    # so check the datastore
-    cleaned_url = clean_url(orig_url)
-    scope = owner ? owner.shortened_urls : self
-    scope.find_or_create_by_url(cleaned_url)
+    unless duplicates
+      # don't want to generate the link if it has already been generated
+      # so check the datastore
+      cleaned_url = clean_url(orig_url)
+      scope = owner ? owner.shortened_urls : self
+      scope.find_or_create_by_url(cleaned_url)
+    end
   end
 
   # return shortened url on success, nil on failure
-  def self.generate(orig_url, owner=nil)
+  def self.generate(orig_url, owner=nil, duplicates=false)
     begin
       generate!(orig_url, owner)
     rescue
